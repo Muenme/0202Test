4b4cf8404b343ce12f0770d6935a9f51d33c288a
«Мэри <muenme.world@gmail.com>
2026-05-11 21:34:59 +0300

    Хз команда пишет, что уже все естб

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d76bb7a55c553e66fa8e3257b32347aca13eb6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
