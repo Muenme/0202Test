3d76bb7a55c553e66fa8e3257b32347aca13eb6e
«Мэри <muenme.world@gmail.com>
2026-05-11 21:00:04 +0300

    CI и тест пробный

---
+++ Test1.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+namespace TestProject
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        private Class1 lib;
+        [TestInitialize]
+        public void Setup()
+        {
+            lib = new Class1();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double n = lib.Addition(1, 2);
+            Assert.AreEqual(n, 3);        
+        }
+    }
+}
