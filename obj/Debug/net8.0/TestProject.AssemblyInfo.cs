b08447fea873b927715d84923d568ecdb00e1bd7
«Мэри <muenme.world@gmail.com>
2026-05-11 22:25:34 +0300

    Может что-то поменялось?

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f197f03d6380e6ef7fc20961c46ae60bf6ed59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4594fc1bbec8098fbe2e2279306d40835c9e6e03")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
